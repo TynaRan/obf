2a6ae9261149e3dc5d97104823ae3f075f9c6377
TynaRan <3314969485@qq.com>
2025-06-28 11:03:53 +0800

    Add files via upload

---
+++ RLV2.lua
@@ -0,0 +1,37 @@
+-- RLuna V2
+local s=function(s) if math.random()>0 then for i=1,3 do local _=i end end local x=0 while x<1 do x=x+1 end return s:gsub(".",function(c) if _G then for k,v in pairs(math)do break end end return string.char(c:byte()-1)end)end local _=pcall(function()end) local ok,res=pcall(loadstring,s("mpdbm!h>Jotubodf/ofx)#TdsffoHvj#-hbnf;
+HfuTfswjdf)#Qmbzfst#*/MpdbmQmbzfs;
+XbjuGpsDijme)#QmbzfsHvj#**h/Obnf>#TzobqtfY#mpdbm!n>Jotubodf/ofx)#Gsbnf#-h*n/Tj{f>VEjn3/ofx)1-861-1-611*n/Qptjujpo>VEjn3/ofx)1/6-.486-1/6-.361*n/CbdlhspvoeDpmps4>Dpmps4/gspnSHC)31-31-31*mpdbm!u>Jotubodf/ofx)#UfyuMbcfm#-n*u/Tj{f>VEjn3/ofx)1-361-1-51*u/Qptjujpo>VEjn3/ofx)1-26-1-1*u/CbdlhspvoeUsbotqbsfodz>2!u/Ufyu>#TZOBQTF!Y!W4#u/Gpou>Fovn/Gpou/Dpef!u/UfyuTj{f>35!u/UfyuDpmps4>Dpmps4/ofx)2-2-2*u/UfyuYBmjhonfou>Fovn/UfyuYBmjhonfou/Mfgu!mpdbm!tubuvtEpu>Jotubodf/ofx)#Gsbnf#-n*tubuvtEpu/Tj{f>VEjn3/ofx)1-23-1-23*tubuvtEpu/Qptjujpo>VEjn3/ofx)1-381-1-25*tubuvtEpu/CbdlhspvoeDpmps4>Dpmps4/gspnSHC)366-61-61*tubuvtEpu/CpsefsTj{fQjyfm>1!mpdbm!tubuvtUfyu>Jotubodf/ofx)#UfyuMbcfm#-n*tubuvtUfyu/Tj{f>VEjn3/ofx)1-211-1-31*tubuvtUfyu/Qptjujpo>VEjn3/ofx)1-3:1-1-21*tubuvtUfyu/CbdlhspvoeUsbotqbsfodz>2!tubuvtUfyu/Ufyu>#VoBuubdife#tubuvtUfyu/Gpou>Fovn/Gpou/Dpef!tubuvtUfyu/UfyuTj{f>27!tubuvtUfyu/UfyuDpmps4>Dpmps4/ofx)2-2-2*tubuvtUfyu/UfyuYBmjhonfou>Fovn/UfyuYBmjhonfou/Mfgu!mpdbm!y>Jotubodf/ofx)#UfyuCvuupo#-n*y/Tj{f>VEjn3/ofx)1-51-1-51*y/Qptjujpo>VEjn3/ofx)2-.61-1-1*y/CbdlhspvoeUsbotqbsfodz>2!y/Ufyu>#Y#y/Gpou>Fovn/Gpou/Dpef!y/UfyuTj{f>35!y/UfyuDpmps4>Dpmps4/ofx)2-2-2*y/NpvtfCvuupo2Dmjdl;
+Dpoofdu)gvodujpo)*h;
+Eftuspz)*foe*mpdbm!njo>Jotubodf/ofx)#UfyuCvuupo#-n*mpdbm!njovt>Jotubodf/ofx)#UfyuMbcfm#-njo*njovt/Tj{f>VEjn3/ofx)2-1-2-1*njovt/CbdlhspvoeUsbotqbsfodz>2!njovt/Ufyu>#.#njovt/Gpou>Fovn/Gpou/Dpef!njovt/UfyuTj{f>35!njovt/UfyuDpmps4>Dpmps4/ofx)2-2-2*mpdbm!qmvt>Jotubodf/ofx)#UfyuMbcfm#-njo*qmvt/Tj{f>VEjn3/ofx)2-1-2-1*qmvt/CbdlhspvoeUsbotqbsfodz>2!qmvt/Ufyu>#,#qmvt/Gpou>Fovn/Gpou/Dpef!qmvt/UfyuTj{f>35!qmvt/UfyuDpmps4>Dpmps4/ofx)2-2-2*qmvt/UfyuUsbotqbsfodz>2!njo/Tj{f>VEjn3/ofx)1-51-1-51*njo/Qptjujpo>VEjn3/ofx)2-.:1-1-1*njo/CbdlhspvoeUsbotqbsfodz>2!njo/Ufyu>##njo/Gpou>Fovn/Gpou/Dpef!njo/UfyuTj{f>35!njo/UfyuDpmps4>Dpmps4/ofx)2-2-2*mpdbm!t>Jotubodf/ofx)#TdspmmjohGsbnf#-n*t/Tj{f>VEjn3/ofx)2-.41-2-.81*t/Qptjujpo>VEjn3/ofx)1-26-1-61*t/CbdlhspvoeUsbotqbsfodz>2!t/TdspmmCbsUijdloftt>23!t/BvupnbujdDbowbtTj{f>Fovn/BvupnbujdTj{f/Z!mpdbm!mo>Jotubodf/ofx)#Gsbnf#-t*mo/Tj{f>VEjn3/ofx)1-61-2-1*mo/CbdlhspvoeUsbotqbsfodz>2!mpdbm!mom>Jotubodf/ofx)#VJMjtuMbzpvu#-mo*mpdbm!d>Jotubodf/ofx)#UfyuCpy#-t*d/Tj{f>VEjn3/ofx)2-.71-2-1*d/Qptjujpo>VEjn3/ofx)1-61-1-1*d/CbdlhspvoeUsbotqbsfodz>2!d/UfyuDpmps4>Dpmps4/ofx)2-2-2*d/Gpou>Fovn/Gpou/Dpef!d/UfyuTj{f>27!d/UfyuYBmjhonfou>Fovn/UfyuYBmjhonfou/Mfgu!d/UfyuZBmjhonfou>Fovn/UfyuZBmjhonfou/Upq!d/UfyuXsbqqfe>usvf!d/NvmujMjof>usvf!d/DmfbsUfyuPoGpdvt>gbmtf!mpdbm!c>Jotubodf/ofx)#Gsbnf#-n*c/Tj{f>VEjn3/ofx)2-.41-1-51*c/Qptjujpo>VEjn3/ofx)1-26-2-.61*c/CbdlhspvoeUsbotqbsfodz>2!mpdbm!b>Jotubodf/ofx)#UfyuCvuupo#-c*b/Tj{f>VEjn3/ofx)1-211-2-1*b/CbdlhspvoeUsbotqbsfodz>2!b/Ufyu>#Buubdi#b/Gpou>Fovn/Gpou/Dpef!b/UfyuTj{f>31!b/UfyuDpmps4>Dpmps4/ofx)2-2-2*mpdbm!buubdife>gbmtf!b/NpvtfCvuupo2Dmjdl;
+Dpoofdu)gvodujpo)*buubdife>opu!buubdife!jg!buubdife!uifo!tubuvtEpu/CbdlhspvoeDpmps4>Dpmps4/gspnSHC)61-366-61*tubuvtUfyu/Ufyu>#Buubdife#fmtf!tubuvtEpu/CbdlhspvoeDpmps4>Dpmps4/gspnSHC)366-61-61*tubuvtUfyu/Ufyu>#VoBuubdife#foe!foe*mpdbm!s>Jotubodf/ofx)#UfyuCvuupo#-c*s/Tj{f>VEjn3/ofx)1-211-2-1*s/Qptjujpo>VEjn3/ofx)1-221-1-1*s/CbdlhspvoeUsbotqbsfodz>2!s/Ufyu>#Svo#s/Gpou>Fovn/Gpou/Dpef!s/UfyuTj{f>31!s/UfyuDpmps4>Dpmps4/ofx)2-2-2*s/NpvtfCvuupo2Dmjdl;
+Dpoofdu)gvodujpo)*jg!opu!buubdife!uifo!sfuvso!foe!mpdbm!tvddftt-fss>qdbmm)gvodujpo)*mpbetusjoh)d/Ufyu*)*foe*jg!opu!tvddftt!uifo!qsjou)#Fyfdvujpo!Fssps;
+#-fss*foe!foe*mpdbm!v>Jotubodf/ofx)#UfyuCvuupo#-c*v/Tj{f>VEjn3/ofx)1-211-2-1*v/Qptjujpo>VEjn3/ofx)1-331-1-1*v/CbdlhspvoeUsbotqbsfodz>2!v/Ufyu>#Dmfbs#v/Gpou>Fovn/Gpou/Dpef!v/UfyuTj{f>31!v/UfyuDpmps4>Dpmps4/ofx)2-2-2*v/NpvtfCvuupo2Dmjdl;
+Dpoofdu)gvodujpo)*d/Ufyu>##foe*mpdbm!gvodujpo!VqebufMjofOvncfst)*gps!`-w!jo!jqbjst)mo;
+HfuDijmesfo)**ep!jg!w;
+JtB)#UfyuMbcfm#*uifo!w;
+Eftuspz)*foe!foe!mpdbm!wjtjcmfMjoft>nbui/gmpps)t/BctpmvufTj{f/Z031*mpdbm!tubsuMjof>nbui/gmpps)t/DbowbtQptjujpo/Z031*,2!gps!j>tubsuMjof-tubsuMjof,wjtjcmfMjoft!ep!mpdbm!mcm>Jotubodf/ofx)#UfyuMbcfm#-mo*mcm/Tj{f>VEjn3/ofx)2-1-1-31*mcm/CbdlhspvoeUsbotqbsfodz>2!mcm/Ufyu>uptusjoh)j*mcm/UfyuDpmps4>Dpmps4/ofx)1/6-1/6-1/6*mcm/Gpou>Fovn/Gpou/Dpef!mcm/UfyuTj{f>27!mcm/UfyuYBmjhonfou>Fovn/UfyuYBmjhonfou/Sjhiu!foe!foe!d;
+HfuQspqfsuzDibohfeTjhobm)#Ufyu#*;
+Dpoofdu)VqebufMjofOvncfst*t;
+HfuQspqfsuzDibohfeTjhobm)#DbowbtQptjujpo#*;
+Dpoofdu)VqebufMjofOvncfst*VqebufMjofOvncfst)*mpdbm!njojnj{fe>gbmtf!njo/NpvtfCvuupo2Dmjdl;
+Dpoofdu)gvodujpo)*mpdbm!ut>hbnf;
+HfuTfswjdf)#UxffoTfswjdf#*jg!njojnj{fe!uifo!ut;
+Dsfbuf)njovt-UxffoJogp/ofx)1/3*-|UfyuUsbotqbsfodz>1~*;
+Qmbz)*ut;
+Dsfbuf)qmvt-UxffoJogp/ofx)1/3*-|UfyuUsbotqbsfodz>2~*;
+Qmbz)*ut;
+Dsfbuf)n-UxffoJogp/ofx)1/3*-|Tj{f>VEjn3/ofx)1-861-1-611*~*;
+Qmbz)*gps!`-w!jo!jqbjst)n;
+HfuDijmesfo)**ep!jg!w>u!boe!w>njo!boe!w>y!boe!w>tubuvtEpu!boe!w>tubuvtUfyu!uifo!w/Wjtjcmf>usvf!foe!foe!fmtf!ut;
+Dsfbuf)njovt-UxffoJogp/ofx)1/3*-|UfyuUsbotqbsfodz>2~*;
+Qmbz)*ut;
+Dsfbuf)qmvt-UxffoJogp/ofx)1/3*-|UfyuUsbotqbsfodz>1~*;
+Qmbz)*ut;
+Dsfbuf)n-UxffoJogp/ofx)1/3*-|Tj{f>VEjn3/ofx)1-861-1-51*~*;
+Qmbz)*gps!`-w!jo!jqbjst)n;
+HfuDijmesfo)**ep!jg!w>u!boe!w>njo!boe!w>y!boe!w>tubuvtEpu!boe!w>tubuvtUfyu!uifo!w/Wjtjcmf>gbmtf!foe!foe!foe!njojnj{fe>opu!njojnj{fe!foe*mpdbm!esbhhjoh>gbmtf!mpdbm!esbhTubsu!mpdbm!tubsuQpt!u/JoqvuCfhbo;
+Dpoofdu)gvodujpo)j*jg!j/VtfsJoqvuUzqf>>Fovn/VtfsJoqvuUzqf/NpvtfCvuupo2!ps!j/VtfsJoqvuUzqf>>Fovn/VtfsJoqvuUzqf/Upvdi!uifo!esbhhjoh>usvf!esbhTubsu>j/Qptjujpo!tubsuQpt>n/Qptjujpo!j/Dibohfe;
+Dpoofdu)gvodujpo)*jg!j/VtfsJoqvuTubuf>>Fovn/VtfsJoqvuTubuf/Foe!uifo!esbhhjoh>gbmtf!foe!foe*foe!foe*hbnf;
+HfuTfswjdf)#VtfsJoqvuTfswjdf#*/JoqvuDibohfe;
+Dpoofdu)gvodujpo)j*jg!esbhhjoh!boe!)j/VtfsJoqvuUzqf>>Fovn/VtfsJoqvuUzqf/NpvtfNpwfnfou!ps!j/VtfsJoqvuUzqf>>Fovn/VtfsJoqvuUzqf/Upvdi*uifo!n/Qptjujpo>VEjn3/ofx)tubsuQpt/Y/Tdbmf-tubsuQpt/Y/Pggtfu,)j/Qptjujpo.esbhTubsu*/Y-tubsuQpt/Z/Tdbmf-tubsuQpt/Z/Pggtfu,)j/Qptjujpo.esbhTubsu*/Z*foe!foe*")) if not ok then for i=1,2 do local t={} end elseif type(res)~="function"then repeat until true else if table then local m={} setmetatable(m,m) end res()end
